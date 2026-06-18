f11fb7192c446192adfc3a7193bcd9585cf8047b
Meksha Tajane <mekshatajane@gmail.com>
2026-06-19 00:03:27 +0530

    Add seaborn dependency to requirements

--- modules/module1_Data_Collection.py
+++ modules/module1_Data_Collection.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#mport seaborn as sns
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import os
  
