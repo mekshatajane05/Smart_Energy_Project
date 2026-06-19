4c62f4340e7da370e81f7f48a8436cde09fc793b
Meksha Tajane <mekshatajane@gmail.com>
2026-06-19 23:14:20 +0530

    load raw dataset and capture missing_before snapshot

---
+++ modules/module2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+print("=" * 60)
+print("  MODULE 2: DATA CLEANING AND PREPROCESSING")
+print("=" * 60)
